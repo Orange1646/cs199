11cdb7aacc8c24d50171bbca1a410f93624b9549
Orange1646 <lijianan19960604@gmail.com>
2017-12-27 14:32:57 -0600

    1
    1

---
+++ Roll a Ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+    private int i = 0;
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        if (i < 10 && i >= 0) {
+            transform.Translate(new Vector3(1, 1, -1) * Time.deltaTime/2);
+            i++;
+        }
+        if(i >= 10)
+        {
+            i = -10;
+        }
+        if(i >= -10 && i < 0)
+        {
+            transform.Translate(new Vector3(-1, -1, 1) * Time.deltaTime/2);
+            i++;
+        }
+    }
+}
